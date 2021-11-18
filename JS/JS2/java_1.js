4810d2084fd2653869422270c7046d61e6db27b4
juraantoniv <juraantoniv@gmail.com>
2021-11-18 21:57:01 +0200

    JS_2

---
+++ JS/JS2/java_1.js
@@ -0,0 +1,82 @@
+// let a=[2,3,4,5,6,7];
+// let b=['dsd','fsff','fdf','fd','fd'];
+
+//console.log(a)
+
+
+// let a=[]
+// a[0]=5;
+// a[1]=6;
+// a[2]='dsssd';
+// a[3]='4';
+// a[4]=true;
+//
+// console.log(a)
+
+// for (let i=0; i<20; i++) {
+//    document.write(`<div>empty text ${i}</div>`)
+// }
+
+// let a=[22,'aa','cc','vv','fssf']
+// for (i=0;i<5;i++){
+//    document.write(`${a[i]} ''`)
+//
+//    }
+
+// let a=[22,'aa','cc','vv','fssf']
+// let n = 0;
+// while (n < 10) {
+//    document.write(`<div>${a[n]} ''</div>`)
+//    n++;
+// }
+
+
+// let a=[22,'aa','cc','vv','fssf',true,false]
+//  let n = 0;
+// while (n <length.a) {
+//    if(typeof a[i]==='boolean') {
+//       document.write(`<div>${a[n]} ''</div>`)
+//       n++;
+//    }
+// }
+
+// let array=[]
+//
+// a[0]=22;
+// a[1]=21;
+// a[2]='dsds';
+// a[3]=4;
+// a[4]=5;
+// a[5]=6;
+// a[6]=5;
+// a[7]=6;
+// a[8]=true;
+//
+// for (let i = 0; i < array.length; i++) {
+//    const arrayElement = array[i];
+//    console.log(arrayElement)
+
+
+// a[0]=22;
+// a[1]=21;
+// a[2]='dsds';
+// a[3]=4;
+// a[4]=5;
+// a[5]=6;
+// a[6]=5;
+// a[7]=6;
+// a[8]=true;
+//
+// for (let i = 0; i < array.length; i+=2) {
+//    const arrayElement = array[i];
+//    console.log(arrayElement)
+// }
+//
+// for (let i = 0; i < 100; i+=2) {
+//    if(i%2===0) {
+//       console.log('Цикл : ' + i + ' ');
+//    }
+//  }
+
+
+
