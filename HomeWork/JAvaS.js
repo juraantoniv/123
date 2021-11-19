7c6e8e823ea0f98bf897419afb6417391d7cfbc8
juraantoniv <juraantoniv@gmail.com>
2021-11-19 20:15:01 +0200

    ClassWork2

---
+++ HomeWork/JAvaS.js
@@ -0,0 +1,36 @@
+ let k=[2,17,13,6,22,31,45,66,100,-18]
+// let elem
+// i=0
+// while (i<=k.length){
+//     elem=k
+//     console.log(k[i])
+//     i++
+// }
+
+// for (let i=0; i<=k.length; i++) {
+//     console.log(k[i])
+// }
+// i=0;
+// while (i<=k.length){
+//    elem=k;
+//     if(elem[i]%2===0){
+//         console.log(elem[i])
+//     }
+//  i++
+// }
+
+ // for (let i=0; i<=k.length; i++) {
+ //  if (k[i]%3===0){
+ // k[i]='Okten';
+ //   console.log(k[i])
+ //  }
+ //
+ // }
+ // for (i = k.length-1; i>=0 ; i--) {
+ //     document.write(k[i] + ' ');
+ // }
+
+ // for (let i=0; i<=k.length; i++) {
+ //     if (i%2===0)
+ // console.log(k[i])
+ // }
