80a27ee4c329b516961b6468c76fb6aee00f76a2
juraantoniv <juraantoniv@gmail.com>
2021-11-23 20:27:54 +0200

    ClassWork3

---
+++ 4/HomeWork/js.js
@@ -0,0 +1,81 @@
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+
+// function max(a,b,c ){
+//    if (a>b && a>c) {
+//         console.log(a)
+//     }
+//     else if (b>a && b>c){
+//         console.log(b)
+//     }
+//     else {
+//         console.log(c)
+//     }
+// }
+//
+// max(5,50,20)
+
+// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+
+// function max(a,b,c ){
+//     if (a<b && a<c) {
+//         console.log(a)
+//     }
+//     else if (b<a && b<c){
+//         console.log(b)
+//     }
+//     else {
+//         console.log(c)
+//     }
+// }
+//
+// max(5,50,20)
+
+// - створити функцію яка повертає найбільше число з масиву
+
+let arr=[5,7,20,15,22,]
+// function max(arr){
+//     let max=arr[0];
+//     for (const arrElement of arr) {
+//         if(arrElement>max)
+//             max=arrElement
+//     }
+//     return max
+// }
+// document.write(max(arr))
+
+// - створити функцію яка повертає найменьше число з масиву
+
+
+// function min(arr){
+//     let min=arr[0];
+//     for (const arrElement of arr) {
+//         if(arrElement<min)
+//             min=arrElement
+//     }
+//     return min
+// }
+//
+// document.write(min(arr))
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+
+// function sum(arr){
+//     let sum=0;
+//     for (const arrElement of arr) {
+//         sum+=arrElement
+//     }
+//     return sum
+// }
+//  document.write(sum(arr))
+
+// - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+
+// function ser(arr){
+//     let ser=0;
+//   for (const arrElement of arr) {
+//       ser+=arrElement/arr.length
+//   }
+//     return ser
+// }
+//  document.write(Math.round(ser(arr)))
+//
